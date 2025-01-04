615162ddbfbe6165895f9485bdc03b9128e7ecda
eunjeong0911 <81670561+eunjeong0911@users.noreply.github.com>
2025-01-04 14:41:47 +0900

    [Bronze III] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,17 @@
+a = input()
+b = input()
+c=list(b)
+
+n = int(a)*int(c[2])
+m = int(a)*int(c[1])
+m1 = str(m) + "0"
+i = int(a)*int(c[0])
+i1 = str(i) + "00"
+
+print(n)
+print(m)
+print(i)
+print(n + int(m1) + int(i1))
+
+
+
